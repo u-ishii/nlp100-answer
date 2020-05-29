4c19356de153904945767cd1a1dedfafc2d5c8df
u-ishii <yuki.ishii.business@gmail.com>
2020-05-29 20:12:51 +0900

    solve 10

---
+++ src/10.ts
@@ -0,0 +1,8 @@
+import {promises as fs} from 'fs'
+
+const solve10 = async (): Promise<number> => {
+    const popularNames: string = await fs.readFile('./res/popular-names.txt', 'utf-8')
+    return (popularNames.match(/\n/g) || []).length + 1
+}
+
+solve10().then(console.log)
