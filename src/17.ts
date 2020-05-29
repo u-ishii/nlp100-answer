54e6b64fc400e8bfefa72b2702d85a7e4c9a0ed5
u-ishii <yuki.ishii.business@gmail.com>
2020-05-29 23:25:18 +0900

    solve 17

---
+++ src/17.ts
@@ -0,0 +1,13 @@
+import { promises as fs } from 'fs'
+import { range } from './share/iterable-helper'
+
+const solve17 = async (): Promise<Array<string>> => {
+    const popularNames: string = await fs.readFile('./res/popular-names.txt', 'utf-8')
+    const popularNameRows = popularNames.split('\n')
+    const headChars = popularNameRows
+        .filter(row => row.length > 0)
+        .map(row => row[0])
+    return [...new Set(headChars)].sort()
+}
+
+solve17().then(console.log)
