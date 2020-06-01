0e89ab1b1ce8599456046eeb23aef5000aca72cd
u-ishii <yuki.ishii.business@gmail.com>
2020-06-01 17:02:45 +0900

    solve 19

---
+++ src/19.ts
@@ -0,0 +1,17 @@
+import { promises as fs } from 'fs'
+
+const solve18 = async (): Promise<any> => {
+    return [
+        ...(await fs.readFile('./res/popular-names.txt', 'utf-8'))
+            .split('\n')
+            .map(row => row.split('\t')[0])
+            .reduce((countMap: Map<string, number>, name) => {
+                countMap.set(name, (countMap.get(name) || 0) + 1)
+                return countMap
+            }, new Map())
+            .entries()
+    ]
+        .sort(([nameA, countA], [nameB, countB]) => countB - countA)
+}
+
+solve18().then(console.log)
