3d1560644269c8fcab2070f9d065db16942d533f
u-ishii <yuki.ishii.business@gmail.com>
2020-07-20 18:18:27 +0900

    fix tox

--- src/share/get_england_text.py
+++ src/share/get_england_text.py
@@ -7,9 +7,6 @@ def get_england_text() -> str:
     return next(
         filter(
             lambda article: article["title"] == "イギリス",
-            map(
-                json.loads,
-                read_gzip_string("./res/jawiki-country.json.gz").split("\n"),
-            ),
+            map(json.loads, read_gzip_string("./res/jawiki-country.json.gz").split("\n"),),
         )
     )["text"]
